eb6727fde155368ba4bb40853b8b89a1869eb0a0
Jose Alvarez Loredo <48250416+EnriqueCS@users.noreply.github.com>
2024-02-16 23:52:50 +0000

    Refactor

--- HashingAlgorithms/C_Data_Testing/HashTableTests.cpp
+++ HashingAlgorithms/C_Data_Testing/HashTableTests.cpp
@@ -39,19 +39,19 @@ void InsertFromFileAndPrintInfo(HashingAlgorithm* hashTable, const std::string& 
 // Adjusted to accept HashingAlgorithm pointer
 void TestEnglishDictionary(HashingAlgorithm* hashTable, const std::string& out) {
     // Ensure the path to the file is correct relative to the executable or use an absolute path
-    InsertFromFileAndPrintInfo(hashTable, "words.txt", "Results/EnglishDict_" + out, 20); // 20 Seconds limit
+    InsertFromFileAndPrintInfo(hashTable, "words.txt", "Results/English_Dict/" + out, 20); // 20 Seconds limit
 }
 void TestRandomEnglish1Dictionary(HashingAlgorithm* hashTable, const std::string& out) {
     // Ensure the path to the file is correct relative to the executable or use an absolute path
-    InsertFromFileAndPrintInfo(hashTable, "mixed_words1.txt", "Results/EnglishDict_R1" + out, 20); // 20 Seconds limit
+    InsertFromFileAndPrintInfo(hashTable, "mixed_words1.txt", "Results/English_DictR1/" + out, 20); // 20 Seconds limit
 }
 void TestRandomEnglish2Dictionary(HashingAlgorithm* hashTable, const std::string& out) {
     // Ensure the path to the file is correct relative to the executable or use an absolute path
-    InsertFromFileAndPrintInfo(hashTable, "mixed_words2.txt", "Results/EnglishDict_R2" + out, 20); // 20 Seconds limit
+    InsertFromFileAndPrintInfo(hashTable, "mixed_words2.txt", "Results/English_DictR2/" + out, 20); // 20 Seconds limit
 }
 void TestRandomEnglish3Dictionary(HashingAlgorithm* hashTable, const std::string& out) {
     // Ensure the path to the file is correct relative to the executable or use an absolute path
-    InsertFromFileAndPrintInfo(hashTable, "mixed_words3.txt", "Results/EnglishDict_R3" + out, 20); // 20 Seconds limit
+    InsertFromFileAndPrintInfo(hashTable, "mixed_words3.txt", "Results/English_DictR3/" + out, 20); // 20 Seconds limit
 }
 
 // Test cases
