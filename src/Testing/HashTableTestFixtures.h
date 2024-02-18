729ad377aa5fed1d87831a1c3e783453372fc841
Jose Alvarez Loredo <48250416+EnriqueCS@users.noreply.github.com>
2024-02-18 09:18:48 +0000

    Perfect atm

--- src/Testing/HashTableTestFixtures.h
+++ src/Testing/HashTableTestFixtures.h
@@ -5,8 +5,24 @@
 
 #include <gtest/gtest.h>
 #include "../Algorithms/HashingAlgorithm.h" // Adjust the path as needed
+#include "nlohmann/json.hpp"
+#include <chrono>
+#include <fstream>
+#include <iostream>
 
 #define SIZE_VARIABLE 10000
+
+// Function to insert data from a file and print hash table information
+void InsertFromFileAndPrintInfo(HashingAlgorithm* hashTable, const std::string& fileName, const std::string& out, int durationInSeconds);
+
+// Function to load JSON data from a file
+nlohmann::json load_test_data(const std::string& jsonFilePath);
+
+// Function to remove the "Data/" prefix from a string
+std::string removeDataPrefix(const std::string& str);
+
+// Function to test dictionaries based on JSON configuration
+void TestDictionaries(HashingAlgorithm* hashTable, const std::string& jsonFilePath, const std::string& testCategory, const std::string& out);
 
 // Test fixture for Chaining
 class ChainingTest : public ::testing::Test {
